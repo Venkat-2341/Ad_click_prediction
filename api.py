2195afecffd540748dce6af414f05c0efe91063c
Venkat <vke.1743@gmail.com>
2024-11-29 11:25:23 +0530

    api working, with model as pipe.pkl rf

--- api.py
+++ api.py
@@ -9,58 +9,34 @@ app = FastAPI(title="Ad click prediction")
 
 class AdDetails(BaseModel):
     Daily_Time_Spent_on_Site: float
-    Age: float
+    Age: int
     Area_Income: float
     Daily_Internet_Usage: float
     Ad_topic_line: str
     City: str
     Male: int
     Country: str
-    timestamp: str
 
-with open('./models/preprocessors.pkl', 'rb') as f:
-    preprocessors = pickle.load(f)
-le_country = preprocessors["le_country"]
-le_city = preprocessors["le_city"]
-le_topic = preprocessors["le_topic"]
-scaler = preprocessors["scaler"]
-
-
-with open('./models/rf_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+with open('./experiments/pipe.pkl', 'rb') as f:
+    pipe = pickle.load(f)
 
 @app.post("/predict")
 def predict(Ad: AdDetails):
     
-    ad_topic_encoded = le_topic.transform([Ad.Ad_topic_line])[0]
-    city_encoded = le_city.transform([Ad.City])[0]
-    country_encoded = le_country.transform([Ad.Country])[0]
-
-    timestamp = datetime.strptime(Ad.timestamp, "%Y-%m-%d %H:%M:%S")  
-    month = timestamp.month
-    day = timestamp.day
-    hour = timestamp.hour
-
-    data_point = np.array([[
+    data_point = np.array([
         Ad.Daily_Time_Spent_on_Site,
         Ad.Age,
         Ad.Area_Income,
         Ad.Daily_Internet_Usage,
-        ad_topic_encoded,
-        city_encoded,
+        Ad.Ad_topic_line,
+        Ad.City,
         Ad.Male,
-        country_encoded,
-        month,
-        day,
-        hour
-    ]])
+        Ad.Country
+    ], dtype=object).reshape(1, -1)
 
-    data_point_scaled = scaler.transform(data_point)
-
-    pred = model.predict(data_point_scaled).tolist()
+    pred = pipe.predict(data_point)
     pred = pred[0]
-    print(pred)
 
     return {
-        "prediction": pred
+        "prediction": int(pred)
     }
